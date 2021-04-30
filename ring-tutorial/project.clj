9fe6613d143c5f45a09f824a670bf1e7d7528151
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-04-30 14:57:37 +0900

    add new dependencies

--- ring-tutorial/project.clj
+++ ring-tutorial/project.clj
@@ -1,4 +1,4 @@
-(defproject ring-demo-001 "0.1.0-SNAPSHOT"
+(defproject todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.9.2"]
                  [compojure "1.4.0"]
+                 [hiccup "1.0.5"]
                  [environ "1.0.1"]]
   :plugins [[lein-environ "1.0.1"]]
   :main ^:skip-aot todo.core
